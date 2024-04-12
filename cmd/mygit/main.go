a8f1166a3df0fe0df5e199bf141a202bcc1c64b4
Nexflare <harshmagarwal@outlook.com>
2024-04-12 12:08:29 -0400

    Hash-obj command added

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"strings"
-
-	// Uncomment this block to pass the first stage!
 
 	"os"
 )
@@ -65,8 +65,49 @@ func main() {
 		content := strings.Split(bytesWriter.String(), "\x00")[1]
 		fmt.Print(content)
 	
+	case "hash-object":
+		if len(os.Args) < 3 {
+			fmt.Fprintf(os.Stderr, "usage: mygit hash-object <file>\n")
+		}
+		writeToObj := false
+		fileName := os.Args[2]
+		if len(os.Args) > 3 && os.Args[2] == "-w" {
+			writeToObj = true
+			fileName = os.Args[3]
+		}
+		fileContent, err := os.ReadFile(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err.Error())
+			os.Exit(1)
+		}
+		blobContent := fmt.Sprintf("blob %d\x00%s", len(string(fileContent)), string(fileContent))
+		hashedValue := getHash([]byte(blobContent))
+		if writeToObj {
+			var b bytes.Buffer
+			writer := zlib.NewWriter(&b)
+			writer.Write([]byte(blobContent))
+			writer.Close()
+			if err := os.MkdirAll(fmt.Sprintf(".git/objects/%s", hashedValue[:2]), 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+			err := os.WriteFile(fmt.Sprintf(".git/objects/%s/%s", hashedValue[:2], hashedValue[2:]), b.Bytes(), 0644)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err.Error())
+				os.Exit(1)
+			}
+		}
+		fmt.Print(hashedValue)
+
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+
+func getHash(data []byte) string {
+	h := sha1.New()
+	h.Write(data)
+	hashedValue := hex.EncodeToString(h.Sum(nil))
+	return hashedValue
 }
